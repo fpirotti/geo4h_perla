3b1a4abc9bb38e0ff8abbdc0d3f6ca9be9618819
fpirotti <fpirotti@gmail.com>
2024-01-22 08:30:56 +0100

    changed outpath and drive download

--- 01_getFromGEE.R
+++ 01_getFromGEE.R
@@ -3,20 +3,39 @@ library(rgeeExtra)
 
 bufferDistance <- 1000L
 
+# ee_Initialize(quiet = T)
+ee_Initialize(user = 'cirgeo' )
 
-ee_Initialize(quiet = T)
-ee_Initialize(user = 'cirgeo')
+options(gargle_verbosity = "debug")
+options(gargle_email= "cirgeo@unipd.it")
+googledrive::drive_auth(email="cirgeo@unipd.it")
 
 
-getCentroids <- ee_utils_pyfunc( function(feature) {
+downloadGDrive <- function(task_vector, outpath="./perla.json"){
+  gd_folder <- basename(ee$batch$Task$status(task_vector)[["destination_uris"]])
+  gd_ExportOptions <- task_vector[["config"]][["fileExportOptions"]]
+  gd_filename <- gd_ExportOptions[["driveDestination"]][["filenamePrefix"]]
+  files_gd <- try(googledrive::drive_find(q = sprintf("'%s' in parents",
+                                                      gd_folder), q = sprintf("name contains '%s'", gd_filename)),
+                  silent = TRUE)
+  if(file.exists(outpath)){
+
+    user_input <- readline( sprintf("Are you sure you want to overwrite path %s? (y/n)  ", outpath ))
+    if(user_input != 'y') stop('Exiting since you did not press y')
+  }
+  googledrive::drive_download(file = files_gd , path =outpath, overwrite = TRUE)
+
+}
+
+getCentroids <- ee_utils_pyfunc(function(feature) {
   ee$feature$set({
     polyCent:feature.centroid()
   })
-} )
+})
 
-bufferPoly <- ee_utils_pyfunc( function(feature) {
+bufferPoly <- ee_utils_pyfunc(function(feature) {
   feature$buffer(bufferDistance)
-} )
+})
 
 
 ### GRAB THE DATA -----
@@ -43,8 +62,7 @@ l7 <-
 
 ### function to do things for the stack of yearly imagery
 
-processYear <-  function(year) {
-
+processYear <- ee_utils_pyfunc(function(year) {
   getNDVI  <-    ee_utils_pyfunc(function(image) {
     nirb <-  image$select(nir)
     redb <-  image$select(red)
@@ -58,30 +76,37 @@ processYear <-  function(year) {
         nirb$subtract(redb)$divide(nirb$add(redb))$addBands(ee$Image(1))$rename('NDVI', "Ncells")
       )
 
-    fin <- fin$updateMask( fin$select("NDVI")$gt(0L) )
+    fin <- fin$updateMask(fin$select("NDVI")$gt(0L))
 
     ff <- fin$reduceRegions(
-      collection = controls, #$filter(ee$Filter$eq('codice', 'berval')),
+      collection = controls,
+      #$filter(ee$Filter$eq('codice', 'berval')),
       reducer = ee$Reducer$sum(),
       scale = 30L
     )
 
-    ff <- fin$select("NDVI")$reduceRegions(
+    ff <- fin$select("NDVI")$rename("stdDevNDVI")$reduceRegions(
       collection = ff,
       reducer = ee$Reducer$stdDev(),
       scale = 30L
     )
-    ff <- ee$Image(cloud)$addBands(ee$Image(1))$rename('Clouds', "NCloudCells")$reduceRegions(
-      collection = ff,
-      reducer = ee$Reducer$sum(),
-      scale = 30L
-    )
+    ff <-
+      ee$Image(cloud)$addBands(ee$Image(1))$rename('Clouds', "NCloudCells")$reduceRegions(
+        collection = ff,
+        reducer = ee$Reducer$sum(),
+        scale = 30L
+      )
 
     ff$map(ee_utils_pyfunc(function(ff) {
-      ff <- ff$set( list("Date"=ee$Image(image)$date()$format('YYYY-MM-dd'),
-                         "Timestamp"=ee$Image(image)$date()$format('YYYY-MM-dd HH:mm:ss'),
-                         "Path"=ee$Image(image)$get('WRS_PATH'),
-                         "Row"=ee$Image(image)$get('WRS_ROW') ) )
+      ff <-
+        ff$set(
+          list(
+            "Date" = ee$Image(image)$date()$format('YYYY-MM-dd'),
+            "Timestamp" = ee$Image(image)$date()$format('YYYY-MM-dd HH:mm:ss'),
+            "Path" = ee$Image(image)$get('WRS_PATH'),
+            "Row" = ee$Image(image)$get('WRS_ROW')
+          )
+        )
     }))
 
 
@@ -100,46 +125,37 @@ processYear <-  function(year) {
     )$map(getNDVI)$flatten()
 
 
-  # prova <-   getNDVI(ee$Image(l1$first()))
+  l1.fd$filter(ee$Filter$gt('stdDev', 0))$filter(ee$Filter$lt('Clouds', 1))
 
-  provaf <- l1.fd$getInfo()
-
-  df <- data.table::rbindlist(lapply(provaf$features, function(x) {
-    x$properties
-  }), fill = TRUE)
-
-  df$Date <- as.Date(df$Date)
-  df$Timestamp <- as.POSIXct(df$Timestamp)
-  df$codice <- as.factor(df$codice)
-
-
-  df.filt <- df %>% dplyr::filter(NDVI!=0)
-  dd <- data.frame(table(df.filt$codice, df.filt$Date))
-
-  ### TM/ETM/ETM+ SENSOR RED AND NIR BAND DESIGNATION ----
-  # nir <- ee$String("B04")
-  # red <- ee$String("B03")
-  # l5.fd <- l5$filterDate(  ee$Number(year)$format("%d")$cat('-03-21'),
-  #                          ee$Number(year)$format("%d")$cat('-10-21') )
-  # l7.fd <- l7$filterDate(  ee$Number(year)$format("%d")$cat('-03-21'),
-  #                          ee$Number(year)$format("%d")$cat('-10-21') )
-
-  l1.fdc
-  # return( list(L1=l1.fd$size(), data=l1.fdc
-  #              # , L5=l5.fd$size()
-  #              # , L7=l7.fd$size()
-  #              ) )
-
-}
+})
 
 
 
 from <- 1972
 to <- 1975
-ndviStack <- ee$List$sequence(from, to)$map(ee_utils_pyfunc(processYear))
+ndviStack <- ee$FeatureCollection( ee$List$sequence(from, to)$map(processYear) )$flatten()
 
 
-all <- (ndviStack$get(0)$getInfo())
+task_vector <- ee_table_to_drive(
+  collection = ndviStack,
+  fileFormat = "GEO_JSON",
+  timePrefix = TRUE,
+  fileNamePrefix = "perla"
+)
+task_vector$start()
+ee_monitoring(task_vector) # optional
+
+downloadGDrive(task_vector)
+
+all <- jsonlite::read_json("perla.json")
+df <- na.omit( data.table::rbindlist(lapply(all$features, function(x) {
+  x$properties
+}), fill = TRUE) )
+
+df$Date <- as.Date(df$Date)
+df$Timestamp <- as.POSIXct(df$Timestamp)
+df$codice <- as.factor(df$codice)
+
 
 names(all) <- sprintf("y%d", from:to)
 
